86667bd9b459119ee74606bf4e8b683a32c0c7d4
SmallChX <minhthuan55588@gmail.com>
2024-03-15 00:02:51 +0700

    call api

--- server/main.go
+++ server/main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"attendanceJF/handler"
+	"attendanceJF/repository"
 	"attendanceJF/settings"
+	"attendanceJF/usecase"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	// gin.SetMode(gin.ReleaseMode)
+
 	err := godotenv.Load()
 	if err != nil {
 		panic("fail to load .env file")
 	}
-	settings.InitDB()
 
+	settings.InitDB()
+	db := settings.GetDB()
+
+	studentRepository := repository.NewStudentRepository(db)
+
+	studentUsecase := usecase.NewStudentUsecase(studentRepository)
+
+	attendanceJFHandler := handler.NewAttendanceJFHandler(studentUsecase)
+
+	router := gin.Default()
+
+	api := router.Group("/api")
+	{
+		students := api.Group("/students")
+		students.GET("/get-attendance-list", attendanceJFHandler.GetAttendanceList)
+		students.GET("/get-checkout-list", attendanceJFHandler.GetCheckOutList)
+		students.PUT("/checkin-out/:id", attendanceJFHandler.HandleCheckInOut)
+		students.GET("/get-lucky-list", attendanceJFHandler.GetLuckyAttendeeList)
+	}
+
+	router.Run(":8080")
 }
